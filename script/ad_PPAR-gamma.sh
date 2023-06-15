7a6025ca5460b25eb88ffafe34f2aa3fef139871
Jindong Li <64452770+jindongli-Ai@users.noreply.github.com>
2023-06-15 17:35:12 +0800

    Update ad_PPAR-gamma.sh

--- script/ad_PPAR-gamma.sh
+++ script/ad_PPAR-gamma.sh
@@ -1,1 +1,1 @@
-python main.py -exp_type ad -DS Tox21_PPAR-gamma -rw_dim 4 -dg_dim 4 -num_epoch 200 -num_cluster 10 -alpha 0.2 -num_layer 6 -GNN_Encoder GCN -graph_level_pool global_mean_pool -eval_freq 5
+python main.py -exp_type ad -DS Tox21_PPAR-gamma -rw_dim 4 -dg_dim 4 -num_epoch 400 -num_cluster 10 -alpha 0.2 -num_layer 6 -GNN_Encoder GCN -lr 0.00001 -graph_level_pool global_mean_pool -eval_freq 5
